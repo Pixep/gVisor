40b67a7297994fa913ed0767c529e8186539669c
Michael Pratt <mpratt@google.com>
2019-06-18 13:31:44 -0700

    Remove O(n) lookup on unlink/rename
    Currently, the path tracking in the gofer involves an O(n) lookup of
    child fidRefs. This causes a significant overhead on unlinks in
    directories with lots of child fidRefs (<4k).
    
    In this transition, pathNode moves from sync.Map to normal synchronized
    maps. There is a small chance of contention in walk, but the lock is
    held for a very short time (and sync.Map also had a chance of requiring
    locking).
    
    OTOH, sync.Map makes it very difficult to add a fidRef reverse map.
    
    PiperOrigin-RevId: 253854848

--- pkg/p9/handlers.go
+++ pkg/p9/handlers.go
@@ -224,7 +224,7 @@ func (t *Tattach) handle(cs *connState) message {
 		file:     sf,
 		refs:     1,
 		mode:     attr.Mode.FileType(),
-		pathNode: &cs.server.pathTree,
+		pathNode: cs.server.pathTree,
 	}
 	defer root.DecRef()
 
@@ -545,15 +545,15 @@ func (t *Tunlinkat) handle(cs *connState) message {
 		// Before we do the unlink itself, we need to ensure that there
 		// are no operations in flight on associated path node. The
 		// child's path node lock must be held to ensure that the
-		// unlink at marking the child deleted below is atomic with
+		// unlinkat marking the child deleted below is atomic with
 		// respect to any other read or write operations.
 		//
 		// This is one case where we have a lock ordering issue, but
 		// since we always acquire deeper in the hierarchy, we know
 		// that we are free of lock cycles.
 		childPathNode := ref.pathNode.pathNodeFor(t.Name)
-		childPathNode.mu.Lock()
-		defer childPathNode.mu.Unlock()
+		childPathNode.opMu.Lock()
+		defer childPathNode.opMu.Unlock()
 
 		// Do the unlink.
 		err = ref.file.UnlinkAt(t.Name, t.Flags)
